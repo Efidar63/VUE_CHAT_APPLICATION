00f3421214e25356af2175a57f9519599dbcdc08
Efidar <94045398+Efidar63@users.noreply.github.com>
2024-02-17 22:02:31 +0300

    firebase/config.js problem still didn't solved

--- chat-app/src/firebase/config.js
+++ chat-app/src/firebase/config.js
@@ -1,23 +1,20 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth'; // for authentication
-import { getFirestore } from 'firebase/firestore'; // for Firestore
-
+import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'; // Import getAuth from firebase/auth
+import { FieldValue } from 'firebase/firestore'; // Import FieldValue from firebase/firestore
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBi2ySpgM1leZAcJ3RUTUQY15m438e3uQ8",
-    authDomain: "blog-vue3-439d9.firebaseapp.com",
-    projectId: "blog-vue3-439d9",
-    storageBucket: "blog-vue3-439d9.appspot.com",
-    messagingSenderId: "742386752632",
-    appId: "1:742386752632:web:dee90881aecdfee2f2e18d"
-  };
+  apiKey: "AIzaSyBi2ySpgM1leZAcJ3RUTUQY15m438e3uQ8",
+  authDomain: "blog-vue3-439d9.firebaseapp.com",
+  projectId: "blog-vue3-439d9",
+  storageBucket: "blog-vue3-439d9.appspot.com",
+  messagingSenderId: "742386752632",
+  appId: "1:742386752632:web:dee90881aecdfee2f2e18d"
+};
+const app = initializeApp(firebaseConfig);
 
-  
-firebase.initializeApp(firebaseConfig);
-
-const projectAuth = firebase.auth();
-const projectFirestore = firebase.firestore();
-const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+const projectAuth = getAuth(app);
+const projectFirestore = getFirestore(app);
+const timestamp = FieldValue.serverTimestamp;
 
 export { projectAuth, projectFirestore, timestamp };
